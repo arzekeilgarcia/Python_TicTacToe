001a096cc89ab67f1c8d13efd51b4e0666fd9f65
Arzekeil De Leon <56529126+arzekeilgarcia@users.noreply.github.com>
2020-11-18 00:14:09 -0500

    Main, Functions
    Functions
    - deleted functions module
    Main
    - Changed condition of while loop for position_chosen() to where it will also keep going if there is already a counter there at that position
    -Created update_board() function that updates the game board

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+#imports
+
+#constants
+
+
 def gameboard(plays_made):
     """
     Purpose:
@@ -20,7 +25,7 @@ def gameboard(plays_made):
     return
 
 
-def position_chosen():
+def position_chosen(plays_made):
     """
     Purpose:
         the function ask the player where they choose to 
@@ -29,22 +34,55 @@ def position_chosen():
         player_choice = position_chosen()
     -----------------------------------------------------
     Parameters:
-        None  
+        plays_made - the list that holds the information 
+        of where players' counters are already placed and 
+        where they aren't 
     Returns:
         player_choice -  the place where the user puts his
         counter
     """
     player_choice = int(input('Enter where you want to place your counter corresponding to the avaliable spaces on the Game Board (1-9): '))
-    while(player_choice > 9 and player_choice < 1):
+    while(player_choice > 9 and player_choice < 1 or plays_made[player_choice - 1] == 'X' or plays_made[player_choice - 1] == 'O'):
         print('Error, You cannot place your counter there')
         player_choice = int(input('Enter where you want to place your counter corresponding to the avaliable spaces on the Game Board (1-9): '))
 
     return player_choice
 
+def update_board(plays_made, player_choice,which_player):
+    """
+    Purpose:
+        updates the game board after the user chooses 
+        where to place their counter on the game board
+    Use:
+        update_board()
+    -----------------------------------------------------
+    Parameters:
+        plays_made - the list that holds the information 
+        of where players' counters are already placed and 
+        where they aren't
+        player_choice - the choice that the player made of 
+        where they wanted their counter to be placed 
+        which_player - identifies which player selected to
+        place down their counter
+    Returns:
+        plays_made - the list that holds the information 
+        of where players' counters are already placed and 
+        where they aren't
+    """
+
+
+    return
+
 def main():
     #plays_made starts with values 1-9 as a way to show what's the original position of each box  
     plays_made = [1,2,3,4,5,6,7,8,9]
+    #prints the game board once for now
     gameboard(plays_made)
+
+    player_choice = position_chosen()
+    update_board(player_choice)
+    #call win condition function 
+    #while loop to keep game running or not
 
 
 #calls the main method
