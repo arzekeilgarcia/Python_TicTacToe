15b9bd81200cf85fe9be250edf0e17e742ac9374
Arzekeil De Leon <56529126+arzekeilgarcia@users.noreply.github.com>
2020-11-17 23:26:30 -0500

    Main
    Main
    - position_chosen function made so that player's can input their choice of where they want to place their counter on the game board

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 def gameboard(plays_made):
     """
     Purpose:
-        Draws the game board for the game TicTacToe
+        Draws the game board for the game Tic Tac Toe
     Use:
         gameboard(plays_made)
     -----------------------------------------------------
     Parameters:
-        plays_made = it is a list as a way to determine
+        plays_made - it is a list as a way to determine
         that holds where a player's piece is held  
     Returns:
         None
@@ -19,7 +19,28 @@ def gameboard(plays_made):
 
     return
 
-    
+
+def position_chosen():
+    """
+    Purpose:
+        the function ask the player where they choose to 
+        to place their counter
+    Use:
+        player_choice = position_chosen()
+    -----------------------------------------------------
+    Parameters:
+        None  
+    Returns:
+        player_choice -  the place where the user puts his
+        counter
+    """
+    player_choice = int(input('Enter where you want to place your counter corresponding to the avaliable spaces on the Game Board (1-9): '))
+    while(player_choice > 9 and player_choice < 1):
+        print('Error, You cannot place your counter there')
+        player_choice = int(input('Enter where you want to place your counter corresponding to the avaliable spaces on the Game Board (1-9): '))
+
+    return player_choice
+
 def main():
     #plays_made starts with values 1-9 as a way to show what's the original position of each box  
     plays_made = [1,2,3,4,5,6,7,8,9]
